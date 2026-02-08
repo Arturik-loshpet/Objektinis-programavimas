bc4048f3ff34db815d24b29cf93b5d79dc06617d
Arturik <artur.vascuk@gmail.com>
2026-02-08 23:02:00 +0200

    Create objektinis.cpp

---
+++ objektinis.cpp
@@ -0,0 +1,51 @@
+#include <iomanip>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <ios>
+
+struct Studentas
+{
+    std::string Var="A", Pav="B";
+    //int *paz;
+    double rez;
+    std::vector<int> paz;
+};
+
+int main(){
+    Studentas A;
+    std::cout << "Iveskite varda ir pavarde: ";
+    std::cin >> A.Var >> A.Pav;
+    std::cout << "Kiek pazymiu turi studentas? ";
+    double n, temp, sum=0;
+    double median;
+    std::cin >> n;
+    for(int i=0; i<n; i++){
+        std::cout << "Ivesk " << i+1 << " is " << n << std::endl;
+        std::cin >> temp;
+        A.paz.push_back(temp);
+        sum += temp;
+    }
+    std::sort(A.paz.begin(), A.paz.end());
+    if(int(n) % 2 == 0){
+        median = (A.paz[n/2 - 1] + A.paz[n/2])/2.0;
+    }
+    else{
+        median = A.paz[int(n/2)];
+    }
+    std::cout << "Koks yra studento egzamino rezultatas? ";
+    std::cin >> A.rez;
+    /*std::cout << "Ka noretumet pamatyt? Mediana - 1, arba Vidurki - 2 ";
+    int a;
+    std::cin >> a;
+    if(a == 1){
+        std::cout << A.Var << " " << A.Pav << " " << median << std::endl;
+    }
+    else{
+        std::cout << std::fixed << std::setprecision(2) << A.Var << " " << A.Pav << " " << (sum / n) * 0.4 + 0.6 * A.rez << std::endl;
+    }*/
+
+    std::cout << std::fixed << "Vardas" << std::setw(10) << "Pavarde" << std::setw(40) << "Galutinis (Vid.) / Galutinis (Med.)" << std::endl;
+    std::cout << "-----------------------------------------------------------------------------------" << std::endl;
+    std::cout << std::fixed << std::setprecision(2) << A.Var << std::setw(10) << A.Pav << std::setw(13) << (sum / n) * 0.4 + 0.6 * A.rez << std::setw(19) << median << std::endl;
+}
