0ebb49e65e006a77e5b7eb8c5471c5a9f9bc119e
Arturik <artur.vascuk@gmail.com>
2026-02-12 00:06:32 +0200

    Dabartinis progress, veliau pakeist, kad pats studentas butu vektorius, kad butu lengviau

--- objektinis-vector.cpp
+++ objektinis-vector.cpp
@@ -9,6 +9,8 @@ struct Studentas{
     std::vector<std::vector<double>> paz;
     std::vector<int> m; //studento pazymiu skaicius
     std::vector<double> egz; //studento egzamino rezultatas
+    std::vector<double> vid;
+    std::vector<double> med;
     
 };
 
@@ -41,13 +43,44 @@ int main(){
         stud.egz.push_back(e);
         std::cout << "Sekantis studentas: " << std::endl;
     }
-    std::cout << std::endl;
+
     for(int i=0; i<n; i++){
+        double sum=0;
+        for(int j=0; j<stud.m[i]; j++){
+            sum+=stud.paz[i][j];
+        }
+        double vid = (sum / stud.m[i] * 1.0) * 0.4 + 0.6 * stud.egz[i];
+        stud.vid.push_back(vid);
+    }
+
+    for(int i=0; i<n; i++){
+        std::sort(stud.paz[i].begin(), stud.paz[i].end());
+        double med;
+        if(stud.m[i] % 2 == 0){
+            med = (stud.paz[i][stud.m[i]/2 - 1] + stud.paz[i][stud.m[i]/2]) / 2.0;
+            stud.med.push_back(med);
+        }
+        else{
+            med = stud.paz[i][stud.m[i]/2];
+            stud.med.push_back(med);
+        }
+    }
+
+    std::cout << std::endl;
+
+    /*for(int i=0; i<n; i++){
         std::cout << stud.vardpav[i] << std::endl;
         for(int j=0; j<stud.m[i]; j++){
             std::cout << stud.paz[i][j] << " ";
         }
+        std::cout<< " egz - " << stud.egz[i];
         std::cout << std::endl;
+    }*/
+
+    std::cout << std::fixed << "Vardas" << std::setw(10) << "Pavarde" << std::setw(20) << "Galutinis (Med.)" << std::endl;
+    std::cout << "-----------------------------------------------------------------------------------" << std::endl;
+    for(int i=0; i<n; i++){
+        std::cout << std::fixed << std::setprecision(2) << stud.vardpav[i] << std::setw(9) << stud.med[i] << std::endl;
     }
     return 0;
 }
