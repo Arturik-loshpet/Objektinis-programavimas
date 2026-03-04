b27572da5798c32a950e62e260dfaaad81ab67eb
Arturik <artur.vascuk@gmail.com>
2026-03-04 17:06:22 +0100

    papildyta

--- objektinis_io.cpp
+++ objektinis_io.cpp
@@ -20,3 +20,44 @@ void paz_sk(Studentas& temp, int& m){
             else break;
         }
 }
+
+void paz_ivestis_ranka(Studentas& temp, int m){
+    for(int j=0; j<m; j++){
+            int pazymis;
+            std::cout << "Iveskite " << j+1 << " pazymi: ";
+            std::cin >> input;
+            pazymis = validation(input);
+            if(pazymis == 0){
+                std::cout << "Ne skaicius!" << std::endl;
+                j--;
+                continue;
+            }
+            else if(pazymis > 10 || pazymis < 1){
+                std::cout << "Netinkamas sk. Bandykite dar karta" << std::endl;
+                j--;
+                continue;
+            }
+            else temp.paz.push_back(pazymis);
+        }
+}
+
+void egz_ivestis_ranka(Studentas& temp){
+    while(true){
+            std::cout << "Koks yra " << temp.vardas << " " <<temp.pavarde << " egzamino rezultatas? ";
+            int egz;
+            std::cin >> input;
+            egz = validation(input);
+            if(egz == 0){
+                std::cout << "Ne skaicius!" << std::endl;
+                continue;
+            }
+            else if(egz > 10 || egz < 1){
+                std::cout << "Netinkamas sk. Bandykite dar karta" << std::endl;
+                continue;
+            }
+            else{
+                temp.egz = egz;
+                break;
+            }
+        }
+}
